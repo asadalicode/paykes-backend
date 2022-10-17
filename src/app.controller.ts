ee300d84b3f1ca35c8526ac8e4f1ba33a400d3d2
bhk1991 <bilalhaider@codesorbit.com>
2022-10-17 17:21:20 +0500

    first commit

---
+++ src/app.controller.ts
@@ -0,0 +1,38 @@
+import { Body, Controller, Get, HttpCode, NotFoundException, Post, Query, Req, Res } from '@nestjs/common';
+import { AppService } from './app.service';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) { }
+
+  @Get('getUsers')
+  getUsers(): any {
+    return this.appService.getUsers();
+  }
+
+
+
+  @Post('updateUser')
+  async updateAuthPassword(@Body() body: any) {
+    const paramKeys = Object.keys(body)
+    const userObj = await this.appService.getUser(paramKeys[0], body[paramKeys[0]]);
+    if (!userObj.length) {
+      throw new NotFoundException('User not found.');
+    }
+    else {
+      const uId = (await this.appService.getUserUid(userObj[0].email)).uid
+      const updateObj = {
+        password: body[paramKeys[1]]
+      }
+      return this.appService.updateAuthPassword(uId, updateObj).then((res) => {
+        this.appService.updateUser(userObj[0], updateObj)
+        return res
+      }).catch((error) => {
+        return error
+      })
+    }
+
+
+
+  }
+}
---
+++ src/app.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@nestjs/common';
+import * as admin from 'firebase-admin';
+import { forkJoin, from, mergeMap } from 'rxjs';
+
+@Injectable()
+export class AppService {
+  async getUsers() {
+    return (await admin.firestore().collection('Users').get()).docs.map(doc => doc.data());
+    // return Promise.all([usersSnap, authUsersSpan])
+
+  }
+
+  async getUser(key, value) {
+    let collectionRef = admin.firestore().collection('Users');
+    let query = collectionRef.where(key, '==', value)
+    return (await query.get()).docs.map(doc => doc.data())
+  }
+
+  async getUserUid(email: string) {
+    return await admin.auth().getUserByEmail(email);
+  }
+
+
+  updateAuthPassword(uid: string, userObj: any) {
+    return admin.auth().updateUser(uid, userObj);
+  }
+
+  updateUser(userCollection: any, userObj: any) {
+    return admin.firestore().collection('Users').doc(userCollection.id).update(userObj);
+  }
+}
